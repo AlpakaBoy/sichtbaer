79b42293382f47308a73ed9344d4a4343d0367d8
AlpakaBoy <105052335+AlpakaBoy@users.noreply.github.com>
2025-03-10 14:26:32 +0100

    Delete backend/authroutes.js

--- backend/authroutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-// Beispiel-Route für Authentifizierung
-router.post("/login", (req, res) => {
-    res.send("Login erfolgreich!");
-});
-
-module.exports = router;
